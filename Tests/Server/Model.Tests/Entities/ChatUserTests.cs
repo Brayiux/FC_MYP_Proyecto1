2629e81e813e1b205d2b3e8a32a96d573c649d19
Brayiux <braymontiel77@gmail.com>
2026-09-16 23:28:17 -0600

    Añadir pruebas unitarias para Model/Entities/ChatUser.cs

---
+++ Tests/Server/Model.Tests/Entities/ChatUserTests.cs
@@ -0,0 +1,90 @@
+﻿using Model.Definitions.Enums;
+using Model.Entities;
+
+namespace Model.Tests.Entities
+{
+    public class ChatUserTests
+    {
+        #region Validación de construcción
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("123456789")]
+        public void UsernameOutOfRangeInConstructorThrowsArgumentOutOfRangeException(string username)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () =>
+                { ChatUser user = new(username); });
+        }
+
+        [Fact]
+        public void UsernameNullInConstructorThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                { ChatUser user = new(null); });
+        }
+
+        [Fact]
+        public void ChatUserFieldsAreConsistentWithConstructor()
+        {
+            ChatUser user = new("Juan");
+
+            Assert.Equal("Juan", user.Username);
+            
+        }
+        [Fact]
+        public void ChatUserStartsActiveAfterConstructor()
+        {
+            ChatUser user = new("Juan");
+
+            Assert.Equal(UserStatus.Active, user.Status);
+
+        }
+        #endregion
+
+        #region Validación de campos y propiedades
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("123456789")]
+        public void SetUsernameOutOfRangeThrowsArgumentOutOfRangeException(string username)
+        {
+            ChatUser user = new("Juan");
+
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () =>
+                { user.Username = username; });
+        }
+
+        [Fact]
+        public void SetUsernameNullThrowsArgumentNullException()
+        {
+            ChatUser user = new("Juan");
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                { user.Username = null; });
+        }
+
+        [Fact]
+        public void SetStatusIsConsistent()
+        {
+            ChatUser user = new("Juan");
+
+            user.Status = UserStatus.Away;
+
+            Assert.Equal(UserStatus.Away, user.Status);
+        }
+
+        [Fact]
+        public void SetTheSameStatusThrowsInvalidOperationException()
+        {
+            ChatUser user = new("Juan") { Status = UserStatus.Away };
+
+            Assert.Throws<InvalidOperationException>(
+                () => { user.Status = UserStatus.Away; });
+        }
+
+        #endregion
+    }
+}
