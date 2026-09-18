6c126f6577d7e9b91c322ab54b0f968eaa710ab5
Brayiux <braymontiel77@gmail.com>
2026-09-17 22:51:50 -0600

    Implementar el punto de entrada ChatServer y HandleMsgsView para recibir notificaciones del controlador

---
+++ Server/ChatServer/ChatServer.cs
@@ -0,0 +1,52 @@
+﻿using ChatServer.Views;
+using Controller;
+using System.Net;
+
+namespace ChatServer;
+
+public class ChatServer
+{
+    private static ServerController? _controller;
+    private static HandleMsgsView? _view;
+    public async static Task Main(string[] args)
+    {
+        string port;
+
+        Console.CancelKeyPress += Console_CancelKeyPress;
+
+        // Lee y valida el puerto en consola o lo inicializamos en 1234 por defecto
+        if (args.Length == 0)
+        {
+            port = "1234";
+        }
+        else if (!int.TryParse(args[0], out int p) || p is < 0 or > 65535)
+        {
+            Console.WriteLine("Error: Puerto no válido.");
+            return;
+        }
+        else
+        {
+            port = args[0];
+        }
+
+        _controller = new(IPAddress.Any, port);
+
+        // Se crea la vista para reaccionar a eventos del controlador
+        _view = new(_controller);
+
+        await _controller.RunAsync();
+
+        _view.Dispose();
+    }
+
+    private static void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+    {
+        e.Cancel = true;
+
+        _controller?.Stop();
+        _view?.Dispose();
+
+        Environment.Exit(0);
+        
+    }
+}
--- Server/ChatServer/Views/HandleMsgsView.cs
+++ Server/ChatServer/Views/HandleMsgsView.cs
@@ -6,14 +6,37 @@ namespace ChatServer.Views
     /// Maneja la vista en consola de mensajes recibidos y enviados
     /// usando el controlador del servidor.
     /// </summary>
-    public class HandleMsgsView
+    public class HandleMsgsView : IDisposable
     {
 
         private readonly ServerController _sc;
 
         public HandleMsgsView(ServerController sc)
         {
-            throw new NotImplementedException();
+            ArgumentNullException.ThrowIfNull(sc);
+
+            _sc = sc;
+
+            _sc.MessageReceived += Sc_MessageReceived;
+            _sc.MessageSent += Sc_MessageSent;
+        }
+
+        private void Sc_MessageSent(string msg)
+        {
+            Console.WriteLine($">> {msg}");
+        }
+
+        private void Sc_MessageReceived(string msg, string from)
+        {
+            Console.WriteLine($"<< {msg}");
+        }
+
+        public void Dispose()
+        {
+            _sc.MessageReceived -= Sc_MessageReceived;
+            _sc.MessageSent -= Sc_MessageSent;
+
+            GC.SuppressFinalize(this);
         }
     }
 }
