d066f827ee8b59b55817937d28e99fd7769a1cd8
Brayiux <braymontiel77@gmail.com>
2026-09-18 22:03:42 -0600

    Implementar PublicMsgSt

--- Server/Controller/Strategies/PublicMsgSt.cs
+++ Server/Controller/Strategies/PublicMsgSt.cs
@@ -1,5 +1,8 @@
-﻿using Controller.Definitions.Abstracts;
+﻿using Controller.Data;
+using Controller.Definitions.Abstracts;
 using Controller.Resources;
+using Model.Entities;
+using Model.Exceptions;
 
 namespace Controller.Strategies
 {
@@ -16,12 +19,50 @@ namespace Controller.Strategies
         }
         public async override Task ExecuteAsync(ClientConnection c)
         {
-            MsgBuilder mb = new();
-
             if (!await HandleClientIdentificationValidationAsync(c))
                 return;
 
+            try
+            {
+                TextRoomRules.ValidateText(_msg);
+                NotifyPublicTextFrom(c);
+
+            }
+            catch (InvalidTextException)
+            {
+                await ReplyInvalidAsync(c);
+
+                ChatData.Instance.RemoveClient(c.Id);
+                ChatData.Instance.RemoveUser(c.User.Username);
+                DisconnectClient(c);
+            }
 
         }
+
+        #region Apoyo
+
+        /// <summary>
+        /// Notifica a varios usuarios de un mensaje de texto enviado
+        /// al chat principal por <paramref name="client"/>.
+        /// </summary>
+        /// <param name="client">Usuario que envía el texto público.</param>
+        private void NotifyPublicTextFrom(ClientConnection client)
+        {
+            MsgBuilder mb = new();
+            string notification = mb.WithType("PUBLIC_TEXT_FROM")
+                                    .WithUsername(client.User.Username)
+                                    .WithText(_msg)
+                                    .Build();
+
+            foreach (var (_, user) in ChatData.Instance.GetAllUsers())
+            {
+                if (!user.Equals(client))
+                {
+                    _ = SendMessageAsync(client, notification);
+                }
+            }
+        }
+
+        #endregion
     }
 }
