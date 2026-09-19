69c8fdc8c121dfa998a6c74c939cfccbb3606eb4
Brayiux <braymontiel77@gmail.com>
2026-09-18 20:35:42 -0600

    Implementar UsersListSt

--- Server/Controller/Strategies/UsersListSt.cs
+++ Server/Controller/Strategies/UsersListSt.cs
@@ -1,5 +1,7 @@
-﻿using Controller.Definitions.Abstracts;
+﻿using Controller.Data;
+using Controller.Definitions.Abstracts;
 using Controller.Resources;
+using Model.Definitions.Enums;
 
 namespace Controller.Strategies
 {
@@ -11,12 +13,30 @@ namespace Controller.Strategies
         }
         public async override Task ExecuteAsync(ClientConnection c)
         {
-            throw new NotImplementedException();
+            MsgBuilder mb = new();
+
+            if (!HandleClientIdentificationValidation(c)) return;
+
+            ReplyUsersList(c, mb);
         }
 
         #region Apoyo
 
+        private void ReplyUsersList(ClientConnection client, MsgBuilder mb)
+        {
 
+            Dictionary<string, UserStatus> users = [];
+            foreach (var (username, user) in ChatData.Instance.GetAllUsers())
+            {
+                users[username] = user.User.Status;
+            } 
+
+            string response = mb.WithType("USER_LIST")
+                                .WithUsers(users)
+                                .Build();
+
+            _ = SendMessageAsync(client, response);
+        }
 
         #endregion
     }
