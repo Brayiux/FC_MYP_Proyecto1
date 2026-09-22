f30acf4b92dd5cfa3a6f5d4a803f64b53083e7b7
Brayiux <braymontiel77@gmail.com>
2026-09-21 19:45:07 -0600

    Añadir enumeración de estados faltante, campo, propiedad y validación de estado para el UserChat

---
+++ Client/Models/Definitions/UserStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.Models.Definitions
+{
+    public enum UserStatus
+    {
+        Active,
+        Away,
+        Bussy
+    }
+}
--- Client/Models/Entities/ChatUser.cs
+++ Client/Models/Entities/ChatUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Client.Models.Definitions;
+using System;
 namespace Client.Models.Entities
 {
     /// <summary>
@@ -6,11 +7,34 @@ namespace Client.Models.Entities
     /// </summary>
     public class ChatUser
     {
+        #region Campos
+
+        /// <summary>
+        /// Estado actual del cliente en el chat.
+        /// </summary>
+        private UserStatus _status;
+
+        #endregion
+
         #region Propiedades
         /// <summary>
         /// Obtiene el nombre del usuario.
         /// </summary>
         public string Username { get; }
+
+        /// <summary>
+        /// Obtiene y modifica el estado actual del usuario.
+        /// </summary>
+        public UserStatus Status
+        {
+            get => _status;
+            set
+            {
+                ValidateStatus(value);
+                _status = value;
+            }
+        }
+
         #endregion
 
         #region Construcción
@@ -45,6 +69,21 @@ namespace Client.Models.Entities
                 throw new ArgumentException(
                     "Error: Nombre de usuario nulo o sale del rango de 1 a 8 caracteres.",
                     nameof(username));
+            }
+        }
+
+        /// <summary>
+        /// Realiza la validación necesaria para el nuevo estado del usuario.
+        /// </summary>
+        /// <param name="newStatus">Estado nuevo para el usuario.</param>
+        /// <exception cref="InvalidOperationException">Si <paramref name="newStatus"/>
+        /// coincide con el estado actual del usuario.</exception>
+        private void ValidateStatus(UserStatus newStatus)
+        {
+            if (newStatus == _status)
+            {
+                throw new InvalidOperationException(
+                    "Error: Estado del usuario modificado al mismo valor.");
             }
         }
 
